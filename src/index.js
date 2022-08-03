8a1355f14954501d0a1855a91438bf166fa0aa62
agychko <agychko@gmail.com>
2022-08-03 10:49:23 +0300

    getAvatar and typing add

--- src/index.js
+++ src/index.js
@@ -9,23 +9,35 @@ const { Server } = require('socket.io');
 
 const io = new Server(server);
 
+const users = [];
+
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/index.html`);
 });
 
 io.on('connection', (socket) => {
   console.log('a user connected');
+  socket.emit('display users', users);
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
 });
 
 io.on('connection', (socket) => {
+  socket.on('new user', (user) => {
+    users.push(user);
+    // io.emit('display users', users);
+    socket.broadcast.emit('add user', user);
+    console.log(users);
+  });
+  socket.on('typing', (data) => {
+    socket.broadcast.emit('add typing', data);
+  });
   socket.on('chat message', (data) => {
     // io.emit('chat message', msg);
     socket.broadcast.emit('other message', data);
     socket.emit('my message', data);
-    console.log(`message to ${data.name}: ${data.msg}`);
+    console.log(`message from ${data.name}: ${data.msg}`);
   });
 });
 
