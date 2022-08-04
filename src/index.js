2bbd3f8744ea9a06e1eccc3b112c2f521b606b39
agychko <agychko@gmail.com>
2022-08-04 15:19:41 +0300

    online-offline status

--- src/index.js
+++ src/index.js
@@ -9,7 +9,13 @@ const { Server } = require('socket.io');
 
 const io = new Server(server);
 
+const getAvatar = () => {
+  const number = Math.floor(Math.random() * 9) + 1;
+  return `https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/chat_avatar_0${number}.jpg`;
+};
+
 const users = [];
+const onlineUsers = [];
 
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/index.html`);
@@ -20,15 +26,34 @@ io.on('connection', (socket) => {
   socket.emit('display users', users);
   socket.on('disconnect', () => {
     console.log('user disconnected');
+    console.log(onlineUsers[socket.id]);
+    if (onlineUsers[socket.id]) {
+      onlineUsers[socket.id].status = 'Offline';
+      // users.push(onlineUsers[socket.id]);
+      delete onlineUsers[socket.id];
+      io.emit('display users', users);
+    }
   });
 });
 
 io.on('connection', (socket) => {
-  socket.on('new user', (user) => {
-    users.push(user);
-    // io.emit('display users', users);
-    socket.broadcast.emit('add user', user);
+  socket.on('new user', (userName) => {
+    const result = users.findIndex((item) => item.userName === userName);
+    if (result === -1) {
+      const status = 'Online';
+      const avatar = getAvatar();
+      onlineUsers[socket.id] = { userName, avatar, status };
+      users.push(onlineUsers[socket.id]);
+      // socket.broadcast.emit('get user', { userName, avatar, status });
+      io.emit('display users', users);
+      socket.emit('get my info', onlineUsers[socket.id]);
+    } else {
+      users[result].status = 'Online';
+      io.emit('display users', users);
+      socket.emit('get my info', users[result]);
+    }
     console.log(users);
+    console.log(onlineUsers);
   });
   socket.on('typing', (data) => {
     socket.broadcast.emit('add typing', data);
