465f97946ff8a99af4fba9d54622a6b0882d786d
agychko <agychko@gmail.com>
2022-08-07 21:20:54 +0300

    any changes

--- src/index.js
+++ src/index.js
@@ -13,11 +13,12 @@ app.use(express.static(`${__dirname}/assets`));
 
 const getAvatar = () => {
   const number = Math.floor(Math.random() * 10) + 1;
-  return `/images/chat_avatar_0${number}.jpg`;
+  return number;
 };
 
 const users = [];
 const onlineUsers = [];
+const messages = [];
 
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/index.html`);
@@ -25,7 +26,7 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
   console.log('a user connected');
-  socket.emit('display users', users);
+  socket.emit('user connected');
   socket.on('disconnect', () => {
     console.log('user disconnected');
     if (onlineUsers[socket.id]) {
@@ -34,7 +35,7 @@ io.on('connection', (socket) => {
       delete onlineUsers[socket.id];
       io.emit('display users', users);
     } else {
-      socket.emit('userExists', 'Sorry, there seems to be an issue with the connection!');
+      socket.emit('error', 'Sorry, there seems to be an issue with the connection!');
     }
   });
 });
@@ -63,22 +64,28 @@ io.on('connection', (socket) => {
     } else {
       socket.emit('error', `${userName} username is taken! Try some other username.`);
     }
+    socket.emit('display users', users);
+    socket.emit('display messages', messages);
     console.log(users);
     console.log(onlineUsers);
+    console.log(messages);
   });
   socket.on('typing', (data) => {
     socket.broadcast.emit('add typing', data);
   });
   socket.on('chat message', (data) => {
+    messages.push(data);
     socket.broadcast.emit('other message', data);
     if (onlineUsers[socket.id]) {
       onlineUsers[socket.id].messageCount += 1;
       socket.emit('my message', data);
       socket.emit('get my info', onlineUsers[socket.id]);
+      io.emit('display users', users);
     } else {
       socket.emit('error', 'Sorry, there seems to be an issue with the connection!');
     }
     console.log(`message from ${data.name}: ${data.msg}`);
+    console.log(messages);
   });
 });
 
